3aaec9a3e0728e94b40c215453236355b2a729bd
John J Kauflin <johnkauflin@gmail.com>
2024-06-29 13:42:46 -0400

    test admin role

--- api/HttpTrigger1.cs
+++ api/HttpTrigger1.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace Johnkauflin.Function
 {
@@ -15,10 +16,20 @@ namespace Johnkauflin.Function
         [FunctionName("HttpTrigger1")]
  //           [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.User, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log,
+             ClaimsPrincipal claimsPrincipal)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            bool userAdmin = claimsPrincipal.IsInRole("jjkadmin");
+/*
+   foreach (Claim claim in claimsPrincipal.Claims)
+   {
+      log.LogInformation("CLAIM TYPE: " + claim.Type + "; CLAIM VALUE: " + claim.Value + "</br>");
+   }
+  */
+
+   // CLAIM TYPE: http://schemas.microsoft.com/2017/07/functions/claims/authlevel; CLAIM VALUE: Admin</br>    
 
             string name = req.Query["name"];
 
@@ -27,7 +38,7 @@ namespace Johnkauflin.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "*** JJK test This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? $"*** JJK test userAdmin = {userAdmin} .This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);

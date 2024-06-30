7e6e8e3bed11239a864026307ec9dfe439503bcf
John J Kauflin <johnkauflin@gmail.com>
2024-06-30 09:22:12 -0400

    test net8

--- api/HttpTrigger1.cs
+++ api/HttpTrigger1.cs
@@ -7,29 +7,17 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Security.Claims;
 
-namespace Johnkauflin.Function
+namespace JohnKauflinWeb.Function
 {
     public static class HttpTrigger1
     {
         [FunctionName("HttpTrigger1")]
- //           [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log,
-             ClaimsPrincipal claimsPrincipal)
+            ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            bool userAdmin = claimsPrincipal.IsInRole("jjkadmin");
-/*
-   foreach (Claim claim in claimsPrincipal.Claims)
-   {
-      log.LogInformation("CLAIM TYPE: " + claim.Type + "; CLAIM VALUE: " + claim.Value + "</br>");
-   }
-  */
-
-   // CLAIM TYPE: http://schemas.microsoft.com/2017/07/functions/claims/authlevel; CLAIM VALUE: Admin</br>    
 
             string name = req.Query["name"];
 
@@ -38,7 +26,7 @@ namespace Johnkauflin.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? $"*** JJK test userAdmin = {userAdmin} .This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
